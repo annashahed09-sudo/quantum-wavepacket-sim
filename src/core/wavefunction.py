7f28a842692e4b6cd48e382c5996a72c44c92d05
Annanash.09 <annashahed09@gmail.com>
2026-06-22 18:18:26 -0400

    Add newline at end of wavefunction.py
    Added a newline at the end of the wavefunction.py file.

--- src/core/wavefunction.py
+++ src/core/wavefunction.py
@@ -7,3 +7,4 @@ def gaussian_wavepacket(x, x0=-10, k0=5, sigma=1.0):
     norm = (1 / (sigma * np.sqrt(np.pi))) ** 0.5
     psi = norm * np.exp(-(x - x0)**2 / (2 * sigma**2)) * np.exp(1j * k0 * x)
     return psi
+    
